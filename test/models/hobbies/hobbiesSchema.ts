4e044032cf7ea2c980d921dc07c719b85b0dbfb4
rsamec <roman.samec2@gmail.com>
2014-09-20 20:51:08 +0200

    JSON schema + annotated metaData for hobbies

--- test/models/hobbies/hobbiesSchema.ts
+++ test/models/hobbies/hobbiesSchema.ts
@@ -11,56 +11,17 @@ var expect = require('expect.js');
 var _:UnderscoreStatic = require('underscore');
 import Q = require('q');
 
-var moment = require('moment');
 
-describe('schema for hobbies', function () {
-    //create test data
-    var data:Hobbies.IHobbiesData;
+//create test data
 
-    var businessRules: any;
-
-    //business rules for hobbies
-    var hobbiesSchema = {
-        "Person": {
-            "type": "object",
-            "properties": {
-                "Checked": { "type": "boolean", "title": "Checked", "default": true },
-                "FirstName": { "type": "string", "title": "First name", "required": "true", "maxLength": "15" },
-                "LastName": { "type": "string", "title": "Last name", "required": "true", "maxLength": "15"},
-                "Contact:": { "type": "object",
-                    "properties": {
-                        "Email": { "type": "string", "title": "Email",
-                            "required": "true",
-                            "maxLength": "100",
-                            "pattern": "S*@S*"
-                        }
-                    }
-                }
-            }
-        },
-        "Hobbies": {
-            "type": "array",
-            "items": {
-                "type": "object",
-                "properties": {
-                    "HobbyName": { "type": "string", "title": "HobbyName", "required": "true", "maxLength": "100"},
-                    "Frequency": { "type": "string", "title": "Frequency", "enum": ["Daily", "Weekly", "Monthly"]},
-                    "Paid": { "type": "boolean", "title": "Paid"},
-                    "Recommedation": { "type": "boolean", "title": "Recommedation"}
-                }
-            },
-            "maxItems": "4",
-            "minItems": "2"
-        }
-    };
-
-
+var sharedHobbiesTest = function(jsonMetaData,jsonSchema) {
+    var data;
+    var businessRules;
     beforeEach(function () {
         //setup
-        data = FormSchema.Util.GetFormValues(hobbiesSchema);
-        businessRules = FormSchema.Util.GetAbstractRule(hobbiesSchema).CreateRule("Main");
+        data = {};
+        businessRules = jsonSchema? new FormSchema.JsonSchemaRuleFactory(jsonMetaData).CreateRule("Main"):new FormSchema.JQueryValidationRuleFactory(jsonMetaData).CreateRule("Main");
     });
-
     describe('hobbies', function () {
 
         describe('first name + last name', function () {
@@ -69,11 +30,11 @@ describe('schema for hobbies', function () {
                 //when
 
                 //exec
-                businessRules.Validate(data);
+                var result = businessRules.Validate(data);
 
                 //verify
-                expect(businessRules.ValidationResult.Errors["Person"].Errors["FirstName"].HasErrors).to.equal(true);
-                expect(businessRules.ValidationResult.Errors["Person"].Errors["LastName"].HasErrors).to.equal(true);
+                expect(result.Errors["Person"].Errors["FirstName"].HasErrors).to.equal(true);
+                expect(result.Errors["Person"].Errors["LastName"].HasErrors).to.equal(true);
             });
 
             it('fill empty names', function () {
@@ -83,11 +44,11 @@ describe('schema for hobbies', function () {
                     LastName: ''
                 };
                 //exec
-                businessRules.Validate(data);
+                var result = businessRules.Validate(data);
 
                 //verify
-                expect(businessRules.ValidationResult.Errors["Person"].Errors["FirstName"].HasErrors).to.equal(true);
-                expect(businessRules.ValidationResult.Errors["Person"].Errors["LastName"].HasErrors).to.equal(true);
+                expect(result.Errors["Person"].Errors["FirstName"].HasErrors).to.equal(true);
+                expect(result.Errors["Person"].Errors["LastName"].HasErrors).to.equal(true);
             });
 
             it('fill long names', function () {
@@ -98,11 +59,11 @@ describe('schema for hobbies', function () {
                 };
 
                 //exec
-                businessRules.Validate(data);
+                var result = businessRules.Validate(data);
 
                 //verify
-                expect(businessRules.ValidationResult.Errors["Person"].Errors["FirstName"].HasErrors).to.equal(true);
-                expect(businessRules.ValidationResult.Errors["Person"].Errors["LastName"].HasErrors).to.equal(true);
+                expect(result.Errors["Person"].Errors["FirstName"].HasErrors).to.equal(true);
+                expect(result.Errors["Person"].Errors["LastName"].HasErrors).to.equal(true);
             });
 
             it('fill some names', function () {
@@ -113,11 +74,56 @@ describe('schema for hobbies', function () {
                 };
 
                 //exec
-                businessRules.Validate(data);
+                var result = businessRules.Validate(data);
 
                 //verify
-                expect(businessRules.ValidationResult.Errors["Person"].Errors["FirstName"].HasErrors).to.equal(false);
-                expect(businessRules.ValidationResult.Errors["Person"].Errors["LastName"].HasErrors).to.equal(false);
+                expect(result.Errors["Person"].Errors["FirstName"].HasErrors).to.equal(false);
+                expect(result.Errors["Person"].Errors["LastName"].HasErrors).to.equal(false);
+            });
+        });
+
+        describe('email', function () {
+
+
+            it('fill no email', function () {
+                //when
+                data = {Person: {Contact: {}}};
+
+                //exec
+                var result = businessRules.Validate(data);
+
+                //verify
+                expect(result.Errors["Person"].Errors["Contact"].Errors["Email"].HasErrors).to.equal(true);
+            });
+
+            it('fill wrong email', function () {
+                //when
+                data = {
+                    Person: { Contact: {
+                        Email: 'jsmith.com' }
+                    }
+                };
+
+                //exec
+                var result = businessRules.Validate(data);
+
+                //verify
+                expect(result.Errors["Person"].Errors["Contact"].Errors["Email"].HasErrors).to.equal(true);
+            });
+
+            it('fill some email', function () {
+                //when
+                data = {
+                    Person: { Contact: {
+                        Email: 'jsmith@gmail.com' }
+                    }
+                };
+
+                //exec
+                var result = businessRules.Validate(data);
+
+                //verify
+                expect(result.Errors["Person"].Errors["Contact"].Errors["Email"].HasErrors).to.equal(false);
             });
         });
 
@@ -128,38 +134,122 @@ describe('schema for hobbies', function () {
                 data.Hobbies = [];
 
                 //exec
-                businessRules.Validate(data);
+                var result = businessRules.Validate(data);
 
                 //verify
-                expect(businessRules.ValidationResult.Errors["Hobbies"].HasErrors).to.equal(true);
+                expect(result.Errors["Hobbies"].HasErrors).to.equal(true);
             });
 
             it('fill two items', function () {
                 //when
                 data.Hobbies = [
-                    {HobbyName:"Skiing"}, {HobbyName:'Chess'}
+                    {HobbyName: "Skiing"},
+                    {HobbyName: 'Chess'}
                 ]
 
                 //exec
-                businessRules.Validate(data);
+                var result = businessRules.Validate(data);
 
                 //verify
-                expect(businessRules.ValidationResult.Errors["Hobbies"].HasErrors).to.equal(false)
+                expect(result.Errors["Hobbies"].HasErrors).to.equal(false)
             });
 
             it('fill five items', function () {
                 //when
                 data.Hobbies = [
-                    {HobbyName:"Skiing"}, {HobbyName:'Chess'}, {HobbyName:"Skiing 2"}, {HobbyName:'Chess 2'}, {HobbyName:"Skiing 3"}
+                    {HobbyName: "Skiing"},
+                    {HobbyName: 'Chess'},
+                    {HobbyName: "Skiing 2"},
+                    {HobbyName: 'Chess 2'},
+                    {HobbyName: "Skiing 3"}
                 ]
 
                 //exec
-                businessRules.Validate(data);
+                var result = businessRules.Validate(data);
 
                 //verify
-                expect(businessRules.ValidationResult.Errors["Hobbies"].HasErrors).to.equal(true)
+                expect(result.Errors["Hobbies"].HasErrors).to.equal(true)
             });
 
         });
     });
+}
+
+describe('JSON schema validation for hobbies', function () {
+
+    var hobbiesSchema = {
+        Person: {
+            type: "object",
+            properties: {
+                Checked: { type: "boolean", title: "Checked", default: true },
+                FirstName: { type: "string", title: "First name", required: true, maxLength: 15 },
+                LastName: { type: "string", title: "Last name", required: true, maxLength: 15 },
+                Contact: {
+                    type: "object",
+                    properties: {
+                        Email: {
+                            type: "string", title: "Email",
+                            required: true,
+                            maxLength: 100,
+                            email: true
+                        }
+                    }
+                }
+            }
+        },
+        Hobbies: {
+            type: "array",
+            items: {
+                type: "object",
+                properties: {
+                    HobbyName: { type: "string", title: "HobbyName", required: true, maxLength: 100 },
+                    Frequency: { type: "string", title: "Frequency", enum: ["Daily", "Weekly", "Monthly"] },
+                    Paid: { type: "boolean", title: "Paid" },
+                    Recommedation: { type: "boolean", title: "Recommedation" }
+                }
+            },
+            maxItems: 4,
+            minItems: 2
+        }
+    };
+
+    sharedHobbiesTest(hobbiesSchema,true);
+
+});
+
+describe('raw data JSON annotated with meta data rules', function () {
+
+    var hobbiesSchema = {
+        Person: {
+            FirstName: {
+                rules: {required: true, maxlength: 15}
+            },
+            LastName: {
+                rules: { required: true, maxlength: 15 }
+            },
+            Contact: {
+                Email: {
+                    rules: {
+                        required: true,
+                        maxlength: 100,
+                        email: true
+                    }
+                }
+            }
+        },
+        Hobbies: [
+            {
+                HobbyName: {
+                    rules: { required: true, maxlength: 100 }
+                },
+                Frequency: {
+                    rules: { enum: ["Daily", "Weekly", "Monthly"]
+                    }
+                }},
+            { maxItems: 4, minItems: 2}
+        ]
+    };
+
+    sharedHobbiesTest(hobbiesSchema,false);
+
 });
